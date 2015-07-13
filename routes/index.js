e33fd86375b156a8ef0199d2230f8f9a58fd180b
marcospalo <marcospalo@gmail.com>
2015-07-13 22:03:07 +0200

    rama creditos
    author

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/author', function(req, res) {
+  res.render('author', { autor: 'Marcos Palomo' });
+});
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
