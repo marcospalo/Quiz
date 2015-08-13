c7caece565a1ee058f23df856eef1483b12493a5
marcospalo <marcospalo@gmail.com>
2015-08-13 21:48:53 +0200

    quiz 17 y 18
    quiz 17 y 18

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,7 @@ router.get('/author', function(req, res) {
 
 //Autoload
 router.param('quizId', quizController.load);
+router.param('commentId', commentController.load);
 
 router.get('/login', sessionController.new);
 router.post('/login', sessionController.create);
@@ -25,13 +26,14 @@ router.get('/login', sessionController.destroy);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes', quizController.index);
-router.get('/quizes/new', quizController.new);
-router.post('/quizes/create', quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
-router.put('/quizes/:quizId(\\d+)', quizController.update);
-router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+router.get('/quizes/new', sessionController.loginRequired, quizController.new);
+router.post('/quizes/create', sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
